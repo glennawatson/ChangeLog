c6ede58983d0ae8b1f99bf325bbdec5f1d99ba7d
Glenn <5834289+glennawatson@users.noreply.github.com>
2020-12-13 02:04:24 +0000

    feature: add dependency text prefix (#52)

--- src/commit-helper.ts
+++ src/commit-helper.ts
@@ -81,6 +81,10 @@ export class CommitHelper {
   ): void {
     if (this.startsWithCaseInsensitive(author, '@dependabot-preview')) {
       this.addItemToResponse(responses, 'Dependencies', commitLine);
+    } else if (this.startsWithCaseInsensitive(message, 'dep')) {
+      this.addItemToResponse(responses, 'Dependencies', commitLine);
+    } else if (this.startsWithCaseInsensitive(message, 'dependency')) {
+      this.addItemToResponse(responses, 'Dependencies', commitLine);
     } else if (this.startsWithCaseInsensitive(message, 'feature')) {
       this.addItemToResponse(responses, 'Features', commitLine);
     } else if (this.startsWithCaseInsensitive(message, 'feat')) {
